c0a387e8aa768900358e962be4131fb7088ac7aa
Lorenzo Medina <Lorenzomedina821@gmail.com>
2026-03-16 11:54:33 -0300

    Se agregan comentarios

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-# main.py
-# IMPORTACIÓN: Solo traemos el Motor.
 from motor import Juego
 
 def ejecutar_programa():
-    # Razonamiento: Creamos la instancia del "Cerebro" del juego.
+    # Se Crea la instancia del "Cerebro" del juego.
     # Al hacer esto, se ejecuta el __init__ de Juego y se crea el equipo.
     partida = Juego()
     
-    # Razonamiento: Llamamos al método que contiene el bucle 'while'.
+    # Se Llamama al método que contiene el bucle 'while'.
     # Aquí es donde el control pasa totalmente al Motor.
     partida.iniciar()
 
-# El "Guardian" del script
 if __name__ == "__main__":
     print("--- 🧊 INICIANDO SISTEMA ICEFORGE 🧊 ---")
     ejecutar_programa()
